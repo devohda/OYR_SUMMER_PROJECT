8a183ffc9f9c49ff695e73577130b7dfc209ce06
devohda <fffvvv6@gmail.com>
2020-07-27 18:25:56 +0900

    aws 서버 db 연결

--- words/server.js
+++ words/server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const port2 = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
@@ -16,20 +17,17 @@ const connection = mysql.createConnection({
     user: conf.user,
     password: conf.password,
     port: conf.port,
-    database: conf.database
-})
+    database: conf.database,
+});
 connection.connect();
 
-app.get('/api/words', (req,res) => {
-    connection.query(
-        "SELECT * FROM words",
-        (err, rows, fields) => {
-
+app.get('/api/words', (req, res) => {
+    connection.query('SELECT * FROM words', (err, rows, fields) => {
+        if (err) throw err;
+        else {
             res.send(rows);
- 
         }
-
-    )
+    });
 });
 
-app.listen(port, () => console.log(`Example app listening at ${port}`))
+app.listen(port2, () => console.log(`Example app listening at ${port2}`));
