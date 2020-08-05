8b12cbaa3740f0d21bd1e3e88c02ab80b27ce1c8
Joe <jin2886480@gmail.com>
2020-08-05 17:32:01 +0900

    홈 화면 css 완성

--- words/client/src/Routes/Home/Home.js
+++ words/client/src/Routes/Home/Home.js
@@ -7,37 +7,39 @@ import searchImg from './search.png';
 
 
 const Main = styled.main`
-    position: relative;
     height:100%;
     display: flex;
     
-    justify-content: center;
-    align-content: center;
     flex-direction : column;
     background-color: #FFFFFF;
 `;
 
 
 const Logo= styled.div`
-    height: 5vw;
+    padding-top: 5vw;
+    padding-bottom: 3vw;
+    padding-right: 2vw;
+    width: 24vw;
+    margin: 0 auto;
 `;
 const Logo_img= styled.img`
-    height: 5vw;
+    width: 22vw;
 `;
 
 
 const Search= styled.div`
     position: relative;
+    width: 40vw;
+    margin: 0 auto;
 `;
 const SearchBox= styled.input`
-    height: 3vw;
-    width: 50vw;
+    height: 2.3vw;
+    width: 40vw;
     border-radius: 3vw;
     border: #dbdbdb 1px solid;
-    padding-left: 2vw;
+    
     font-size: 0.9vw;
-    margin-left: 4vw;
-
+    text-align:center;
     &:focus {
         outline: none;
     }
@@ -46,31 +48,69 @@ const SearchImg = styled.img`
     width : 1.6vw;
     position: absolute;
     right: 2vw;
-    top: 0.5vw;
+    top: 0.4vw;
+    margin: 0 auto;
 `;
 
+
+const ListArea = styled.div`
+    display : flex;
+    flex-direction: row;
+    width: 40vw;
+    margin: 0 auto;
+    justify-content: space-between;
+
+    padding-top: 10vw;
+
+`;
+const ListDiv = styled.div`
+    width : 13vw;
+    display : flex;
+    flex-direction: column;
+
+
+`;
+const ListName = styled.h3`
+    color : #f4b33d;
+    padding-bottom: 3vw;
+    font-size: 1vw;
+
+
+`;
+const List = styled.li`
+    padding-bottom: 0.7vw;
+    
+
+`;
 
 class Home extends React.Component {
 
     render(){
         return (
-            <Main>
-              <Logo>
-                <Link to="/">
-                    <Logo_img src={logo} alt={'home_logo'} />
-                </Link>
-              </Logo>    
+               <Main>
+                <Logo>
+                    <Link to="/">
+                        <Logo_img src={logo} alt={'home_logo'} />
+                    </Link>
+                </Logo>
               <Search>
                 <SearchBox type="text" />
                 <SearchImg src={searchImg} alt={'search'}></SearchImg>
               </Search>
-              <p>이런 단어는 어떠신가요?</p>
-              <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-              </ul>
+              <ListArea>
+                <ListDiv>
+                    <ListName>이런 단어는 어떠신가요?</ListName>
+                    <List></List>
+                    <List></List>
+                    <List></List>
+                </ListDiv>
+                <ListDiv>
+                    <ListName>최근 검색한 단어</ListName>
+                    <List></List>
+                    <List></List>
+                    <List></List>
+                </ListDiv>
+              </ListArea>
             </Main>
         )
       }
