d409bdab7fed4a2767effd28bca8b1220e0a2bfd
devohda <fffvvv6@gmail.com>
2020-08-06 00:53:07 +0900

    HomeWord.js 삭제

--- words/client/src/Routes/Home/Home.js
+++ words/client/src/Routes/Home/Home.js
@@ -1,123 +1,107 @@
 //home 화면.
 import React from 'react';
-<<<<<<< HEAD
-import HomeWord from './HomeWord.js';
-=======
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import logo from './home_logo.png';
 import searchImg from './search.png';
->>>>>>> origin/yejin/home
-
 
 const Main = styled.main`
-    height:100%;
+    height: 100%;
     display: flex;
-    
-    flex-direction : column;
-    background-color: #FFFFFF;
+
+    flex-direction: column;
+    background-color: #ffffff;
 `;
 
-
-const Logo= styled.div`
+const Logo = styled.div`
     padding-top: 5vw;
     padding-bottom: 3vw;
     padding-right: 2vw;
     width: 24vw;
     margin: 0 auto;
 `;
-const Logo_img= styled.img`
+const Logo_img = styled.img`
     width: 22vw;
 `;
 
-
-const Search= styled.div`
+const Search = styled.div`
     position: relative;
     width: 40vw;
     margin: 0 auto;
 `;
-const SearchBox= styled.input`
+const SearchBox = styled.input`
     height: 2.3vw;
     width: 40vw;
     border-radius: 3vw;
     border: #dbdbdb 1px solid;
-    
+
     font-size: 0.9vw;
-    text-align:center;
+    text-align: center;
     &:focus {
         outline: none;
     }
 `;
 const SearchImg = styled.img`
-    width : 1.6vw;
+    width: 1.6vw;
     position: absolute;
     right: 2vw;
     top: 0.4vw;
     margin: 0 auto;
 `;
 
-
 const ListArea = styled.div`
-    display : flex;
+    display: flex;
     flex-direction: row;
     width: 40vw;
     margin: 0 auto;
     justify-content: space-between;
 
     padding-top: 10vw;
-
 `;
 const ListDiv = styled.div`
-    width : 13vw;
-    display : flex;
+    width: 13vw;
+    display: flex;
     flex-direction: column;
-
-
 `;
 const ListName = styled.h3`
-    color : #f4b33d;
+    color: #f4b33d;
     padding-bottom: 3vw;
     font-size: 1vw;
-
-
 `;
 const List = styled.li`
     padding-bottom: 0.7vw;
-    
-
 `;
 
 class Home extends React.Component {
-
-    render(){
+    render() {
         return (
-               <Main>
+            <Main>
                 <Logo>
                     <Link to="/">
                         <Logo_img src={logo} alt={'home_logo'} />
                     </Link>
                 </Logo>
-              <Search>
-                <SearchBox type="text" />
-                <SearchImg src={searchImg} alt={'search'}></SearchImg>
-              </Search>
-              <ListArea>
-                <ListDiv>
-                    <ListName>이런 단어는 어떠신가요?</ListName>
-                    <List></List>
-                    <List></List>
-                    <List></List>
-                </ListDiv>
-                <ListDiv>
-                    <ListName>최근 검색한 단어</ListName>
-                    <List></List>
-                    <List></List>
-                    <List></List>
-                </ListDiv>
-              </ListArea>
+                <Search>
+                    <SearchBox type="text" />
+                    <SearchImg src={searchImg} alt={'search'}></SearchImg>
+                </Search>
+                <ListArea>
+                    <ListDiv>
+                        <ListName>이런 단어는 어떠신가요?</ListName>
+                        <List></List>
+                        <List></List>
+                        <List></List>
+                    </ListDiv>
+                    <ListDiv>
+                        <ListName>최근 검색한 단어</ListName>
+                        <List></List>
+                        <List></List>
+                        <List></List>
+                    </ListDiv>
+                </ListArea>
             </Main>
-        )
-      }
+        );
+    }
 }
 
 export default Home;
